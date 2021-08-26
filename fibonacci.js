36021f0d875d3f37196d97893852b34a8c5b54e0
Zulma Rocio <zrmartinezg@gmail.com>
2021-08-26 18:57:15 -0500

    Add fibonnacci problem in js

---
+++ fibonacci.js
@@ -0,0 +1,27 @@
+// without dynamic programming
+// recursion 
+
+const recursionFibonacci = (n) => {
+  if (n <= 2) return 1;
+  return recursionFibonacci(n - 1) + recursionFibonacci(n - 2);
+}
+
+
+// console.log(recursionFibonacci(6));
+// console.log(recursionFibonacci(10));
+// console.log(recursionFibonacci(100));
+
+// with dynamic programming
+// memoization
+
+const memoFibonacci = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = memoFibonacci(n - 1, memo) + memoFibonacci( n - 2, memo);
+  return memo[n]; 
+}
+
+// console.log(memoFibonacci(6));
+// console.log(memoFibonacci(10));
+// console.log(memoFibonacci(100));
+
